a90cf8d129e518858b720b30c4d0bc3c69a961d8
Artem <artem0fedoseev15@mail.ru>
2024-07-09 16:30:55 +0300

    Components

---
+++ src/app/component-b/component-b.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-b',
+  templateUrl: './component-b.component.html',
+  styleUrls: ['./component-b.component.css']
+})
+export class ComponentBComponent {
+  counter = 0;
+
+  changeCount(type:boolean){
+    type ? this.counter++ : this.counter-- ;
+  }
+}
