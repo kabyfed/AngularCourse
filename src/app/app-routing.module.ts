1c14c17fd3a686097d386bb5baa5b9dd62efea8c
Artem <artem0fedoseev15@mail.ru>
2024-07-12 18:15:47 +0300

    Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageModule } from './page/page.module';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'page',
+    loadChildren: () => import('./page/page.module').then(m => m.PageModule)
+  },
+  {
+    path:'main',
+    component:MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
