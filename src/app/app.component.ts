6ece98dbbba745c4c36f0819a309b451b82f73b2
Artem <artem0fedoseev15@mail.ru>
2024-07-15 14:08:36 +0300

    SeoMetaTags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FirstEvoApp';
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) {
+  }
+
+  ngOnInit(): void {
+    this.title.setTitle("Desk Page");
+    this.meta.addTag({ property: 'og:desc', content: 'root_desc' })
+  }
 }
