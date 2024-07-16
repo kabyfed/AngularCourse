0d1c03e6e8d578eae009a7d4e2551faff2721e3b
Artem <artem0fedoseev15@mail.ru>
2024-07-16 14:13:13 +0300

    UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig} from '@ngx-formly/core';
+import Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'FirstEvoApp';
+  form = new FormGroup({});
+  model = { email: '', password: '' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        label: 'Email',
+        placeholder: 'Введите ваш email',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      templateOptions: {
+        label: 'Password',
+        placeholder: 'Введите пароль',
+        required: true,
+      },
+    },
+  ];
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Form Data:', this.model);
+      Notiflix.Notify.success('Форма успешно отправлена!');
+    } else {
+      Notiflix.Notify.failure('Пожалуйста, заполните все обязательные поля!');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyMaterialModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
